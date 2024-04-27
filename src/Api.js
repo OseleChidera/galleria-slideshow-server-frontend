23e6a46845e0151372eef3a43d2e707ba3195654
Osele Chidera <104494952+OseleChidera@users.noreply.github.com>
2024-04-27 19:13:11 +0100

    Update Api.js
    Updated Api.js api route to use express url from glitch.me

--- src/Api.js
+++ src/Api.js
@@ -1,6 +1,6 @@
 
 export const loaderFunction = async ()=>{
-   const fetchUrl = 'https://galleria-slideshow-server.onrender.com/data'
+   const fetchUrl = 'https://galleria-slideshow-server.glitch.me/data'
    const res = await fetch(fetchUrl ,  {method: "GET", headers: {"Content-Type": "application/json;charset=UTF-8"}})
    const data  = await res.json()
    return data;
